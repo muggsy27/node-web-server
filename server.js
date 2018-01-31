04ca5139136e553d0b998295935170078d555477
Derek Fletes <derekfletes@gmail.com>
2018-01-30 21:49:28 -0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((request, response, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${request.method} ${request.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (error) => {
+    if (error) {
+      console.log('unable to append server.log.');
+    }
+  });
+  next();
+});
+
+// app.use((request, response, next) => {
+//   response.render('maintenance.hbs')
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (request, response) => {
+  response.render('home.hbs', {
+    title: 'Homepage',
+    welcomeMessage: 'What the fuck is up!'
+  });
+});
+
+app.get('/about', (request, response) => {
+  response.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
+});
+
+app.get('/bad', (request, response) => {
+  response.send({
+    error: 'error handling request'
+  });
+});
+
+app.listen(3000, () => {
+  console.log('server is up on port 3000');
+});
